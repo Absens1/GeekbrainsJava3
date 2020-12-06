d63da7fc7875f5bf103f2dd2a38aeae390fa2717
Daniil <ha.li.fa@yandex.ru>
2020-12-06 12:35:44 +0300

    completing first and second tasks

--- src/lesson1/Main.java
+++ src/lesson1/Main.java
@@ -1,4 +1,27 @@
 package lesson1;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
+
+    public static void main(String[] args) {
+        Integer[] intArray = {1, 2, 3, 4, 5};
+        String[] strArray = {"one","two","three","four","five"};
+        System.out.println(Arrays.toString(swapArrayElements(intArray,0,4)));
+        System.out.println(Arrays.toString(swapArrayElements(strArray,0,4)));
+        System.out.println(convertArrayToList(intArray));
+    }
+
+    public static <T> List<T> convertArrayToList(T[] array) {
+        return Arrays.asList(array);
+    }
+
+    public static <T> T[] swapArrayElements(T[] array, int firstIndex, int secondIndex) {
+        T firstValue = array[firstIndex];
+        T secondValue = array[secondIndex];
+        array[firstIndex] = secondValue;
+        array[secondIndex] = firstValue;
+        return array;
+    }
 }
